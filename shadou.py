5cb6d200fc9dfa0783b8cc88de4ffdd33dd096dc
knd2 <gjz.knd2@gmail.com>
2015-02-27 21:36:24 +0800

    user_from

--- shadou.py
+++ shadou.py
@@ -2,7 +2,7 @@
 from douban_client import DoubanClient
 import requests
 
-USERFROM = "knd2"
+# 首次获取的图书信息数
 COUNT = 100
 
 API_KEY = '01270ca17245f891037525e456d1e9b3'
@@ -10,6 +10,7 @@ API_SECRET = 'c4732649acfecebb'
 #API_KEY = '09d2c50567d6e5491d784f103784afdb'
 #API_SECRET = 'c194f8fccc17a325'
 REDIRECT_URI = 'http://jzguo.com/callback_dou'
+# 权限设置
 SCOPE = 'douban_basic_common,book_basic_r,book_basic_w'
 
 class ShaDou(object):
@@ -27,20 +28,23 @@ class ShaDou(object):
         self.books.extend(data)
 
     def getAllBooks(self):
+        u"保存原账户读书信息"
+        user_from = raw_input('please input your original username:')
         start = 0
         self.books = []
-        ret = self.client.book.getAllBooks(USERFROM, start, COUNT)
+        ret = self.client.book.getAllBooks(user_from, start, COUNT)
         self._saveBooks(ret)
         total = int(ret.get('total', 0))
         start = start+int(ret.get('count', 0))
         total = total-int(ret.get('count', 0))
         while total>0:
-            ret = self.client.book.getAllBooks(USERFROM, start, total)
+            ret = self.client.book.getAllBooks(user_from, start, total)
             total = total-int(ret.get('count', 0))
             start = start+int(ret.get('count', 0))
             self._saveBooks(ret)
 
     def setAllBooks(self):
+        u"设置新账户读书信息"
         left = len(self.books)
         while self.books:
             print left, 'book[s] left'
