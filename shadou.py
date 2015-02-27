510ed2af00195128bd4e7169cda66c3439250abb
knd2 <gjz.knd2@gmail.com>
2015-02-27 15:28:13 +0800

    fork douban_client

--- shadou.py
+++ shadou.py
@@ -1,9 +1,19 @@
 # -*- coding: utf8 -*-
+from douban_client import DoubanClient
 import requests
 
+USERFROM = "knd2"
+USERTO = "bt404"
 BOOK = 1
 MOVIE = 2
 MUSIC = 3
+
+API_KEY = '01270ca17245f891037525e456d1e9b3'
+API_SECRET = 'c4732649acfecebb'
+REDIRECT_URI = 'http://jzguo.com/callback_dou'
+SCOPE = 'movie_basic,movie_basic_r,movie_basic_w,thing_basic_r,
+         thing_basic_w,book_basic_r,book_basic_w,music_basic_r,
+         music_basic_w,music_artist_r'
 
 class ShaDou(object):
     u"豆瓣帐号信息复制"
@@ -18,7 +28,7 @@ class ShaDou(object):
         print ret
 
     def login(self)
-        pass
+        client = DoubanClient(API_KEY, API_SECRET, REDIRECT_URI, SCOPE)
 
 
 if __name__ == "__main__":
